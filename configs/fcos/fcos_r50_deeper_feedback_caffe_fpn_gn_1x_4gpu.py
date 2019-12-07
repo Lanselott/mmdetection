79b60055a4ea42b7f55938da9d6a6bdcb90de3f9
Lanselott <chenran1995@link.cuhk.edu.hk>
2019-12-07 15:35:01 +0800

    update

--- configs/fcos/fcos_r50_deeper_feedback_caffe_fpn_gn_1x_4gpu.py
+++ configs/fcos/fcos_r50_deeper_feedback_caffe_fpn_gn_1x_4gpu.py
@@ -103,7 +103,7 @@ data = dict(
 # optimizer
 optimizer = dict(
     type='SGD',
-    lr=0.01,
+    lr=0.0025,
     momentum=0.9,
     weight_decay=0.0001,
     paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
