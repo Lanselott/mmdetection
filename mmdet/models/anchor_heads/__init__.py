9368fb0e312b718223ced77722c1c759ad8eb064
Lanselott <chenran1995@link.cuhk.edu.hk>
2019-12-08 21:54:06 +0800

    updatev2

--- mmdet/models/anchor_heads/__init__.py
+++ mmdet/models/anchor_heads/__init__.py
@@ -14,11 +14,12 @@ from .fcos_gradient_assign_head import FCOSGradientAssignHead
 from .fair_loss_assign_head import FairLossAssignHead
 from .fcos_convention_assign_head import FCOSConventionAssignHead
 from .fcos_deeper_feedback_head import FCOSDeeperFeedbackHead
+from .fcos_deeper_feedback_head_v2 import FCOSDeeperFeedbackHeadV2
 from .fcos_merged_head import FCOSMergedHead
 
 __all__ = [
     'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
     'GARPNHead', 'RetinaHead', 'GARetinaHead', 'SSDHead', 'FCOSHead',
     'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead', 'FCOSRandomAssignHead', 
-    'FCOSGradientAssignHead', 'FairLossAssignHead', 'FCOSConventionAssignHead', 'FCOSDeeperFeedbackHead', 'FCOSMergedHead'
+    'FCOSGradientAssignHead', 'FairLossAssignHead', 'FCOSConventionAssignHead', 'FCOSDeeperFeedbackHead', 'FCOSMergedHead', 'FCOSDeeperFeedbackHeadV2'
 ]
