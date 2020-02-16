a3160925ea5d0962d58b6ad6a7c3011943ff0744
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-02-16 15:03:12 +0800

    update

--- mmdet/models/anchor_heads/ddb_multi_bd_head.py
+++ mmdet/models/anchor_heads/ddb_multi_bd_head.py
@@ -539,6 +539,7 @@ class DDBMultiBDHead(nn.Module):
         mlvl_centerness = torch.cat(mlvl_centerness)
         mlvl_bd_scores = torch.cat(mlvl_bd_scores)
         mlvl_bd_score_factors = torch.cat(mlvl_bd_score_factors)
+        '''
         det_bboxes, det_labels = multiclass_nms_sorting(
             mlvl_bboxes,
             mlvl_scores,
@@ -556,7 +557,6 @@ class DDBMultiBDHead(nn.Module):
             cfg.nms,
             cfg.max_per_img,
             score_factors=mlvl_centerness)
-        '''
         
         return det_bboxes, det_labels
 
