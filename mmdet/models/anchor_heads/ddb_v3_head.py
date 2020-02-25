915986215a2c002f3a88b29c1e6bb3090486ca5c
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-02-25 14:35:07 +0800

    update

--- mmdet/models/anchor_heads/ddb_v3_head.py
+++ mmdet/models/anchor_heads/ddb_v3_head.py
@@ -25,6 +25,7 @@ class DDBV3Head(nn.Module):
                  regress_ranges=((-1, 64), (64, 128), (128, 256), (256, 512),
                                  (512, INF)),
                  mask_origin_bbox_loss=False,
+                 iou_delta=0.0,
                  loss_cls=dict(
                      type='FocalLoss',
                      use_sigmoid=True,
@@ -48,6 +49,7 @@ class DDBV3Head(nn.Module):
         self.strides = strides
         self.regress_ranges = regress_ranges
         self.mask_origin_bbox_loss = mask_origin_bbox_loss
+        self.iou_delta = iou_delta
         self.loss_cls = build_loss(loss_cls)
         self.loss_bbox = build_loss(loss_bbox)
         self.loss_centerness = build_loss(loss_centerness)
@@ -375,13 +377,13 @@ class DDBV3Head(nn.Module):
             '''
             # NOTE: the grad of sorted branch is in sort order, diff from origin
             '''
-            sort_gradient_mask = (_bd_sort_iou > _bd_iou).float()
+            sort_gradient_mask = (_bd_sort_iou > (_bd_iou + self.iou_delta)).float()
             sort_gradient_mask[..., 0] = sort_gradient_mask[pos_gradient_update_mapping[..., 0], 0] 
             sort_gradient_mask[..., 1] = sort_gradient_mask[pos_gradient_update_mapping[..., 1], 1] 
             sort_gradient_mask[..., 2] = sort_gradient_mask[pos_gradient_update_mapping[..., 2], 2] 
             sort_gradient_mask[..., 3] = sort_gradient_mask[pos_gradient_update_mapping[..., 3], 3] 
             
-            origin_gradient_mask = (_bd_sort_iou <= _bd_iou).float()
+            origin_gradient_mask = (_bd_sort_iou <= (_bd_iou + self.iou_delta)).float()
             
             # apply hook to mask origin/sort gradients 
             pos_decoded_sort_bbox_preds.register_hook(lambda grad: grad * sort_gradient_mask)
