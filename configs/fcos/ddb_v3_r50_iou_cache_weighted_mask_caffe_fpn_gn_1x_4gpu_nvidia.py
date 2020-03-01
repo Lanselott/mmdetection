24de5df3ff2f56cc2a7214c28be3294c208428b3
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-03-02 01:55:36 +0800

    update

--- configs/fcos/ddb_v3_r50_iou_cache_weighted_mask_caffe_fpn_gn_1x_4gpu_nvidia.py
+++ configs/fcos/ddb_v3_r50_iou_cache_weighted_mask_caffe_fpn_gn_1x_4gpu_nvidia.py
@@ -29,7 +29,6 @@ model = dict(
         mask_origin_bbox_loss=False,
         iou_delta = 0.0,
         apply_iou_cache=False,
-        mask_sort=False,
         weighted_mask=True,
         consistency_weight=False,
         loss_cls=dict(
