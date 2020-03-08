0c2349ec9500394289e6b05a2bcd7ed76d4d2144
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-03-08 19:53:50 +0800

    update

--- configs/fcos/ddb_v3_x101_multi_scale_range_iou_cache_64x4d_caffe_fpn_gn_2x_4gpu_nvidia.py
+++ configs/fcos/ddb_v3_x101_multi_scale_range_iou_cache_64x4d_caffe_fpn_gn_2x_4gpu_nvidia.py
@@ -69,7 +69,7 @@ train_pipeline = [
     dict(
         type='Resize',
         # img_scale=[(1333, 640), (1333, 800)],
-        img_scale=[(1333, 640), (1333, 800), (1333, 960)],#v2
+        img_scale=[(1333, 640), (1333, 960)],#v2
         # img_scale=[(1333, 640), (1333, 800), [1333, 960]],#v3
         multiscale_mode='range',
         keep_ratio=True),
