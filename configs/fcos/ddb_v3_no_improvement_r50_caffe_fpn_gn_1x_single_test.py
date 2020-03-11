3a9b3e916ba95ef347c4780fe9ff0cbaf149648f
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-03-12 04:00:09 +0800

    update

--- configs/fcos/ddb_v3_no_improvement_r50_caffe_fpn_gn_1x_single_test.py
+++ configs/fcos/ddb_v3_no_improvement_r50_caffe_fpn_gn_1x_single_test.py
@@ -26,7 +26,7 @@ model = dict(
         stacked_convs=4,
         feat_channels=256,
         strides=[8, 16, 32, 64, 128],
-        mask_origin_bbox_loss=True,
+        mask_origin_bbox_loss=False,
         iou_delta=0.0,
         apply_iou_cache=False,
         origin_bbox_loss_downgrade=True,
@@ -39,8 +39,8 @@ model = dict(
             gamma=2.0,
             alpha=0.25,
             loss_weight=1.0),
-        loss_bbox=dict(type='GIoULoss', loss_weight=1.0),
-        loss_sorted_bbox=dict(type='GIoULoss', loss_weight=1.0),
+        loss_bbox=dict(type='IoULoss', loss_weight=1.0),
+        loss_sorted_bbox=dict(type='IoULoss', loss_weight=1.0),
         loss_dist_scores=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
         loss_centerness=dict(
