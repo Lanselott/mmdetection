0544c7f71d51c05fbad0292568ed8a3d9d3917c3
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-03-14 02:57:59 +0800

    fix require grad bug in logits

--- mmdet/models/anchor_heads/ddb_v3_no_improvement_head.py
+++ mmdet/models/anchor_heads/ddb_v3_no_improvement_head.py
@@ -429,7 +429,7 @@ class DDBV3NPHead(nn.Module):
                 pos_decoded_bbox_preds.register_hook(
                     lambda grad: grad * origin_gradient_mask)
             elif self.mask_origin_bbox_loss:
-                if ious_weights.mean() >= 0.6: # default:0.4
+                if ious_weights.mean() >= 0.8: # default:0.4
                     origin_gradient_mask = torch.zeros_like(origin_gradient_mask)
 
                 pos_decoded_bbox_preds.register_hook(
