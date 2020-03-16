3d218b352d235b5f190baec14a75014018338ab9
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-03-16 19:29:10 +0800

    update

--- configs/fcos/fcos_t_s_full_mask_finetune_halved_from_scratch_pyramid_alignment_with_head_logits_distill_r50_caffe_fpn_gn_1x_4gpu_nvidia.py
+++ configs/fcos/fcos_t_s_full_mask_finetune_halved_from_scratch_pyramid_alignment_with_head_logits_distill_r50_caffe_fpn_gn_1x_4gpu_nvidia.py
@@ -65,7 +65,7 @@ model = dict(
         gamma = 2,
         adap_distill_loss_weight = 0.3,
         strides=[8, 16, 32, 64, 128],
-        pyramid_hint_loss=dict(type='MSELoss', loss_weight=10),
+        pyramid_hint_loss=dict(type='MSELoss', loss_weight=15),
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
