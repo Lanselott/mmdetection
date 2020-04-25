fd3c6adcfa02eda8cfe6fb726c4f19f4915d9326
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-04-26 02:37:42 +0800

    :)

--- configs/fcos/fcos_t_s_full_mask_finetune_halved_from_scratch_discriminator_r50_caffe_fpn_gn_1x_4gpu_nvidia.py
+++ configs/fcos/fcos_t_s_full_mask_finetune_halved_from_scratch_discriminator_r50_caffe_fpn_gn_1x_4gpu_nvidia.py
@@ -164,7 +164,7 @@ data = dict(
 #     momentum=0.9,
 #     weight_decay=0.0001,
 #     paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
-optimizer = dict(type='Adam', lr=0.001, betas=(0.9, 0.999), eps=1e-08)
+optimizer = dict(type='Adam', lr=0.00001, betas=(0.9, 0.999), eps=1e-08)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
