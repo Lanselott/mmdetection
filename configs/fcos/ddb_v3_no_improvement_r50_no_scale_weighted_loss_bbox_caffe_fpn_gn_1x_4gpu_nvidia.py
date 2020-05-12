663731ca2f90b5975848ce8928517f4aa9243ec2
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-05-13 02:05:59 +0800

    update

--- configs/fcos/ddb_v3_no_improvement_r50_no_scale_weighted_loss_bbox_caffe_fpn_gn_1x_4gpu_nvidia.py
+++ configs/fcos/ddb_v3_no_improvement_r50_no_scale_weighted_loss_bbox_caffe_fpn_gn_1x_4gpu_nvidia.py
@@ -35,7 +35,7 @@ model = dict(
         box_weighted=True,
         no_scale=False,
         hook_debug=True,
-        sorted_warmup=500,
+        sorted_warmup=1000,
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
@@ -127,7 +127,7 @@ optimizer_config = dict(grad_clip=None)
 lr_config = dict(
     policy='step',
     warmup='constant',
-    warmup_iters=500,
+    warmup_iters=1000,
     warmup_ratio=1.0 / 3,
     step=[8, 11])
 checkpoint_config = dict(interval=1)
