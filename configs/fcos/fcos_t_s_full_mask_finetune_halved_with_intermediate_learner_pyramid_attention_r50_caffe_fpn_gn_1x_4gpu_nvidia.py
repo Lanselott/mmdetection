1641d6ca27c3b7a2ce723333acc8b88f2fdd6e6b
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-06-05 02:34:01 +0800

    update

--- configs/fcos/fcos_t_s_full_mask_finetune_halved_with_intermediate_learner_pyramid_attention_r50_caffe_fpn_gn_1x_4gpu_nvidia.py
+++ configs/fcos/fcos_t_s_full_mask_finetune_halved_with_intermediate_learner_pyramid_attention_r50_caffe_fpn_gn_1x_4gpu_nvidia.py
@@ -71,8 +71,8 @@ model = dict(
         pyramid_hint_loss=dict(type='MSELoss', loss_weight=1),
         reg_head_hint_loss=dict(type='MSELoss', loss_weight=1),
         cls_head_hint_loss=dict(type='MSELoss', loss_weight=1),
-        intermediate_channel=448,
-        i_norm_cfg=dict(type='GN', num_groups=28, requires_grad=True),
+        intermediate_channel=320,
+        i_norm_cfg=dict(type='GN', num_groups=20, requires_grad=True),
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
