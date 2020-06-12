26aeffaca8da28667aa3eebf31802f1bdfd5b990
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-06-12 13:48:31 +0800

    update

--- mmdet/models/anchor_heads/fcos_t_s_full_mask_head.py
+++ mmdet/models/anchor_heads/fcos_t_s_full_mask_head.py
@@ -732,8 +732,10 @@ class FCOSTSFullMaskHead(nn.Module):
             s_i_x = s_x
 
             for t_i_pyramid_align_conv in self.t_i_pyramid_align:
+                # t_i_x = t_i_pyramid_align_conv(
+                #     t_i_x.detach())  # no update to teacher backbone
                 t_i_x = t_i_pyramid_align_conv(
-                    t_i_x.detach())  # no update to teacher backbone
+                    t_i_x)  # update to teacher backbone
             for s_i_pyramid_align_conv in self.s_i_pyramid_align:
                 s_i_x = s_i_pyramid_align_conv(s_i_x)
 
