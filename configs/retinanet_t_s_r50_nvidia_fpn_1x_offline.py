df7e64fb3ae12076ae1effc7cd640e6aa55f7a8f
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-06-15 20:21:24 +0800

    update

--- configs/retinanet_t_s_r50_nvidia_fpn_1x_offline.py
+++ configs/retinanet_t_s_r50_nvidia_fpn_1x_offline.py
@@ -55,6 +55,7 @@ model = dict(
         s_feat_channels=128,
         octave_base_scale=4,
         scales_per_octave=3,
+        finetune_student=True,
         anchor_ratios=[0.5, 1.0, 2.0],
         anchor_strides=[8, 16, 32, 64, 128],
         target_means=[.0, .0, .0, .0],
