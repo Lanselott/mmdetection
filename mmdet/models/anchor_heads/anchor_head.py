544df6c210c68cb15a938dfe1950c65fb4e7dc60
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-09-14 00:11:43 +0800

    update

--- mmdet/models/anchor_heads/anchor_head.py
+++ mmdet/models/anchor_heads/anchor_head.py
@@ -248,12 +248,9 @@ class AnchorHead(nn.Module):
             if self.norm_pyramid:
                 s_x_feats = F.normalize(s_x_feats, p=2, dim=1)
                 x_feats = F.normalize(x_feats, p=2, dim=1)
-            if self.adapt_on_channel:
-                pyramid_hint_loss = pyramid_lambda * self.pyramid_hint_loss(
-                    s_x_feats, x_feats)
-            else:
-                pyramid_hint_loss = pyramid_lambda * self.pyramid_hint_loss(
-                    s_x_feats, x_feats.detach())
+            
+            pyramid_hint_loss = pyramid_lambda * self.pyramid_hint_loss(
+                s_x_feats, x_feats.detach())
 
             if self.pyramid_wise_attention:
                 bbox_num_pos = bbox_weights.reshape(self.num_anchors, -1,
@@ -270,16 +267,10 @@ class AnchorHead(nn.Module):
                         1 - bbox_distance / bbox_distance.max()).detach()
                     attention_weight *= anchors_weights
 
-                    if self.adapt_on_channel:
-                        pos_attention_pyramid_hint_loss = attention_lambda * self.pyramid_hint_loss(
-                            s_x_feats[pos_bbox_inds],
-                            x_feats[pos_bbox_inds],
-                            weight=attention_weight)
-                    else:
-                        pos_attention_pyramid_hint_loss = attention_lambda * self.pyramid_hint_loss(
-                            s_x_feats[pos_bbox_inds],
-                            x_feats[pos_bbox_inds].detach(),
-                            weight=attention_weight)
+                    pos_attention_pyramid_hint_loss = attention_lambda * self.pyramid_hint_loss(
+                        s_x_feats[pos_bbox_inds],
+                        x_feats[pos_bbox_inds].detach(),
+                        weight=attention_weight)
 
                 else:
                     pos_attention_pyramid_hint_loss = t_pos_bbox_pred.sum()
