cb519789934e3a100a35bb286726327d72f977b6
Lanselott <chenran1995@link.cuhk.edu.hk>
2020-09-22 00:31:52 +0800

    update

--- mmdet/models/anchor_heads/anchor_head.py
+++ mmdet/models/anchor_heads/anchor_head.py
@@ -183,7 +183,7 @@ class AnchorHead(nn.Module):
                 # attention_lambda = 500 + 20 * (self.train_step // 7330)
                 pyramid_lambda = 100
             else:
-                attention_lambda = 12.0 / (
+                attention_lambda = 15.0 / (
                     1.0 + math.exp(-2 * (self.train_step // 7330 - 1)))
                 # pyramid_lambda = 8 / (1 + 0.33 * self.train_step // 7330)
                 pyramid_lambda = 8.0
@@ -192,8 +192,8 @@ class AnchorHead(nn.Module):
                 attention_lambda = 2000
                 pyramid_lambda = 100
             else:
-                attention_lambda = 12.0
-                pyramid_lambda = 8.0
+                attention_lambda = 15.0
+                pyramid_lambda = 1.0
 
         if type(cls_score) is tuple:
             t_cls_score = cls_score[0].permute(0, 2, 3, 1).reshape(
@@ -276,7 +276,7 @@ class AnchorHead(nn.Module):
                                               s_pos_bbox_pred).sum(1)
                     attention_weight = (
                         1 - bbox_distance / bbox_distance.max()).detach()
-                    attention_weight *= anchors_weights
+                    # attention_weight *= anchors_weights
 
                     pos_attention_pyramid_hint_loss = attention_lambda * self.pyramid_hint_loss(
                         s_x_feats[pos_bbox_inds],
